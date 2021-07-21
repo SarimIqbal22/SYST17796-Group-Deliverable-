378818ac1e9723e3e5f07bef15b4cd26bdc83d33
Abigail Aubé <84865580+abbyaube@users.noreply.github.com>
2021-07-21 19:07:35 -0400

    Add files via upload

---
+++ src/cardgame/CardHandGenerator.java
@@ -0,0 +1,29 @@
+package cardgame;
+
+import java.util.Random;
+
+public class CardHandGenerator {
+
+    /**
+     * 
+     * @param numCards
+     */
+    public static Card[] generateHand(int numCards) {
+       //an array to hold 7 cards
+        Card[] hand=new Card[numCards];
+        
+        Random random=new Random();
+        
+        for(int i=0; i< hand.length;i++)
+        {
+           
+            Value value=Value.values()[random.nextInt(13)];
+            Suit suit=Suit.values()[random.nextInt(4)];
+            Card card=new Card(value,suit);
+            hand[i]=card;
+        
+        }
+        return hand;
+    }
+
+}
